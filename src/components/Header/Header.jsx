58e45287f4db5616bd0a7eb9eb7a2a2d0d1866ac
gpopowic <gmpopowich@gmail.com>
2024-11-01 16:07:26 -0700

    installed typewriter, styled image and text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,40 @@
+import { Typewriter } from "react-simple-typewriter";
+
 function Header() {
   return (
     <>
-      <section className="home w-screen h-screen"></section>
-      Header
+      <section className="home w-screen h-screen flex">
+        <header
+          className="header flex justify-center h-full w-full content-center my-8"
+          id="home"
+        >
+          {/* Image */}
+          <div className="image-container w-6/12 h-1/2 flex justify-center">
+            <img
+              src="../../../public/assets/images/headshot_adrian.jpg"
+              alt="image of Adrian"
+              className="w-6/12 rounded-full"
+            />
+          </div>
+          {/* Greeting */}
+          <div className="greeting-container w-6/12">
+            <p className="greeting">Hello, I am</p>
+            <h1 className="intro text-5xl">Adrian</h1>
+            <h2 className="subtitle-main text-2xl">
+              {" "}
+              <Typewriter
+                words={[" Production Designer.", "Director."]}
+                loop
+                cursor
+                cursorStyle="|"
+                typeSpeed={10}
+                deleteSpeed={10}
+                delaySpeed={1000}
+              />
+            </h2>
+          </div>
+        </header>
+      </section>
     </>
   );
 }
