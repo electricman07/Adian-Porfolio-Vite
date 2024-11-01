884be6253a8e0f4ff2b8e1dca3c0981112ff16d8
gpopowic <gmpopowich@gmail.com>
2024-11-01 15:34:41 -0700

    setup basic navbar and styled

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,23 @@
+import "./Navbar.css";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className="main-navbar h-12 w-screen flex justify-between items-center sticky top-0 px-5">
+        <div className="left-section">
+          <div className="logo-container">
+            <h2 className="text-xl">Header logo</h2>
+          </div>
+        </div>
+        <div className="right-section flex text-justify gap-4">
+          <div className="">link</div>
+          <div className="">link</div>
+          <div className="">link</div>
+          <div className="">link</div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
