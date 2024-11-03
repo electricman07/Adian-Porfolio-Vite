849591234e5cd688e97062241a7230864b576305
gpopowic <gmpopowich@gmail.com>
2024-11-03 12:56:55 -0800

    added slider code

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -1,15 +1,36 @@
 import images from "./images";
 
+const ImageSlider = ({ images }) => {
+  const settings = {    
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+
 function Portfolio() {
   return (
     <>
       <section
-        className="portfolio-container h-screen flex justify-items-center flex-col"
+        className="portfolio-container h-screen"
         id="portfolio"
       >
         <div className="portfolio-header text-3xl text-center ">
           <h2>Portfolio</h2>
         </div>
+        <div className="image-slider">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Slide ${index}`} />
+          </div>
+        ))}
+      </Slider>
+    </div>
       </section>
     </>
   );
