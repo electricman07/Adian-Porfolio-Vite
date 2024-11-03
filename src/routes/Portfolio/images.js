714086fd4ea720193d72e098deb14ea8a15dedc6
gpopowic <gmpopowich@gmail.com>
2024-11-03 12:08:13 -0800

    copied images file to Portfolio folder

---
+++ src/routes/Portfolio/images.js
@@ -0,0 +1,56 @@
+const images = [
+  {
+    id: 1,
+    image: "/assets/images/header-background-image-1.jpg",
+  },
+  {
+    id: 2,
+    image: "/assets/images/header-background-image-2.jpg",
+  },
+  {
+    id: 3,
+    image: "/assets/images/header-background-image-3.jpg",
+  },
+  {
+    id: 4,
+    image: "/assets/images/header-background-image-4.jpg",
+  },
+  {
+    id: 5,
+    image: "/assets/images/header-background-image-5.jpg",
+  },
+  {
+    id: 6,
+    image: "/assets/images/header-background-image-6.jpg",
+  },
+  {
+    id: 7,
+    image: "/assets/images/header-background-image-7.jpg",
+  },
+  {
+    id: 8,
+    image: "/assets/images/header-background-image-8.jpg",
+  },
+  {
+    id: 9,
+    image: "/assets/images/header-background-image-9.jpg",
+  },
+  {
+    id: 10,
+    image: "/assets/images/header-background-image-10.jpg",
+  },
+  {
+    id: 11,
+    image: "/assets/images/header-background-image-11.jpg",
+  },
+  {
+    id: 12,
+    image: "/assets/images/header-background-image-12.jpg",
+  },
+  {
+    id: 13,
+    image: "/assets/images/header-background-image-13.jpg",
+  },
+];
+
+export default images;
