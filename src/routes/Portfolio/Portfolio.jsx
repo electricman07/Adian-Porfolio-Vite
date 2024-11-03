9bac0c2bf1fe988833da59b1ac617c49e327d3ad
gpopowic <gmpopowich@gmail.com>
2024-11-03 12:18:42 -0800

    added basic skelton of portfolio header and slider container

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -1,5 +1,14 @@
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <>
+      <section className="portfolio-container " id="portfolio">
+        <div className="portfolio-header">
+          <h2>Portfolio</h2>
+        </div>
+        <div className="portfolio-carousel-container"></div>
+      </section>
+    </>
+  );
 }
 
 export default Portfolio;
