835f070c17dcb9820709db7e2b081edcf5cb9c44
gpopowic <gmpopowich@gmail.com>
2024-11-03 12:51:22 -0800

    removed react-slideshow-image as could not configure it to center images

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -1,38 +1,14 @@
-import { Fade } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
 import images from "./images";
-
-const spanStyle = {
-  padding: "20px",
-  background: "#efefef",
-  color: "#000000",
-};
-
-const divStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundSize: "cover",
-  height: "400px",
-};
 
 function Portfolio() {
   return (
     <>
-      <section className="portfolio-container" id="portfolio">
+      <section
+        className="portfolio-container h-screen flex justify-items-center flex-col"
+        id="portfolio"
+      >
         <div className="portfolio-header text-3xl text-center ">
           <h2>Portfolio</h2>
-        </div>
-
-        <div className="slide-container">
-          <Fade>
-            {images.map((fadeImage, index) => (
-              <div key={index}>
-                <img style={{ ...divStyle }} src={fadeImage.image} />
-                <span style={spanStyle}>{fadeImage.caption}</span>
-              </div>
-            ))}
-          </Fade>
         </div>
       </section>
     </>
