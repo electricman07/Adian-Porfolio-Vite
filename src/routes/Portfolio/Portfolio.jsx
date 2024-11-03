c4ea798bf6da1b4e6f193159d6475ce040ffa9d5
gpopowic <gmpopowich@gmail.com>
2024-11-03 13:11:46 -0800

    added styling for slider and caption section

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -1,36 +1,40 @@
-import images from "./images";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const ImageSlider = ({ images }) => {
-  const settings = {    
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-  };
+import images from "./images";
+import "./Portfolio.css";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  cssEase: "linear",
+};
 
 function Portfolio() {
   return (
     <>
-      <section
-        className="portfolio-container h-screen"
-        id="portfolio"
-      >
-        <div className="portfolio-header text-3xl text-center ">
+      <section className="portfolio-container h-screen" id="portfolio">
+        <div className="portfolio-header text-3xl text-center mb-10">
           <h2>Portfolio</h2>
         </div>
         <div className="image-slider">
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt={`Slide ${index}`} />
-          </div>
-        ))}
-      </Slider>
-    </div>
+          <Slider {...settings}>
+            {images.map((image, index) => (
+              <div key={index}>
+                <img src={image.image} alt={`Slide ${index}`} />
+                <div className="text-center">
+                  <h3>{image.caption}</h3>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </section>
     </>
   );
