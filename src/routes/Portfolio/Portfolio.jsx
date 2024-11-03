f6eae5792db187c627fb3261e4e8158fa4851b14
gpopowic <gmpopowich@gmail.com>
2024-11-03 12:42:58 -0800

    added styling for captions and image

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -2,6 +2,20 @@ import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import images from "./images";
 
+const spanStyle = {
+  padding: "20px",
+  background: "#efefef",
+  color: "#000000",
+};
+
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundSize: "cover",
+  height: "400px",
+};
+
 function Portfolio() {
   return (
     <>
@@ -9,17 +23,16 @@ function Portfolio() {
         <div className="portfolio-header text-3xl text-center ">
           <h2>Portfolio</h2>
         </div>
-        <div className="portfolio-carousel-container">
-          <div className="slide-container">
-            <Fade>
-              {images.map((fadeImage, index) => (
-                <div key={index}>
-                  <img style={{ width: "100%" }} src={fadeImage.url} />
-                  <h2>{fadeImage.caption}</h2>
-                </div>
-              ))}
-            </Fade>
-          </div>
+
+        <div className="slide-container">
+          <Fade>
+            {images.map((fadeImage, index) => (
+              <div key={index}>
+                <img style={{ ...divStyle }} src={fadeImage.image} />
+                <span style={spanStyle}>{fadeImage.caption}</span>
+              </div>
+            ))}
+          </Fade>
         </div>
       </section>
     </>
