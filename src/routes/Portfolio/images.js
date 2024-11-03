3671285c9d9e014a3d63281973d04130481113c6
gpopowic <gmpopowich@gmail.com>
2024-11-03 12:28:24 -0800

    added caption section to file

--- src/routes/Portfolio/images.js
+++ src/routes/Portfolio/images.js
@@ -2,54 +2,67 @@ const images = [
   {
     id: 1,
     image: "/assets/images/header-background-image-1.jpg",
+    caption: "image 1",
   },
   {
     id: 2,
     image: "/assets/images/header-background-image-2.jpg",
+    caption: "image 2",
   },
   {
     id: 3,
     image: "/assets/images/header-background-image-3.jpg",
+    caption: "image 3",
   },
   {
     id: 4,
     image: "/assets/images/header-background-image-4.jpg",
+    caption: "image 4",
   },
   {
     id: 5,
     image: "/assets/images/header-background-image-5.jpg",
+    caption: "image 5",
   },
   {
     id: 6,
     image: "/assets/images/header-background-image-6.jpg",
+    caption: "image 6",
   },
   {
     id: 7,
     image: "/assets/images/header-background-image-7.jpg",
+    caption: "image 7",
   },
   {
     id: 8,
     image: "/assets/images/header-background-image-8.jpg",
+    caption: "image 8",
   },
   {
     id: 9,
     image: "/assets/images/header-background-image-9.jpg",
+    caption: "image 9",
   },
   {
     id: 10,
     image: "/assets/images/header-background-image-10.jpg",
+    caption: "image 10",
   },
   {
     id: 11,
     image: "/assets/images/header-background-image-11.jpg",
+    caption: "image 11",
   },
   {
     id: 12,
     image: "/assets/images/header-background-image-12.jpg",
+    caption: "image 12",
   },
   {
     id: 13,
     image: "/assets/images/header-background-image-13.jpg",
+    caption: "image 13",
   },
 ];
 
