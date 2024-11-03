3b97a57f25388816369d8ebf75cebdaadeb95558
gpopowic <gmpopowich@gmail.com>
2024-11-03 12:36:03 -0800

    configured slider code

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -2,20 +2,6 @@ import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import images from "./images";
 
-const spanStyle = {
-  padding: "20px",
-  background: "#efefef",
-  color: "#000000",
-};
-
-const divStyle = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundSize: "cover",
-  height: "400px",
-};
-
 function Portfolio() {
   return (
     <>
