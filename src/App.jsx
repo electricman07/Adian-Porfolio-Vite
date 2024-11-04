44ef648d4e5ac078ffbff1c5e3d6e5e150768bb9
gpopowic <gmpopowich@gmail.com>
2024-11-04 12:54:38 -0800

    added Contact section to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import MainSection from "./routes/MainSection/MainSection";
 import About from "./routes/About/About";
 import Portfolio from "./routes/Portfolio/Portfolio";
+import Contact from "./routes/Contact/Contact";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <MainSection />
       <About />
       <Portfolio />
+      <Contact />
     </>
   );
 }
