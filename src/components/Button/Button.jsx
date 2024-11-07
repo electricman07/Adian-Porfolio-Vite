f83eb415a20f8cd75ffbeb924de4e5cc7bb00728
gpopowic <gmpopowich@gmail.com>
2024-11-07 09:10:24 -0800

    adjusted font size of icon with inline style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ function Button({ CV, buttonName, classList, buttonImage }) {
   return (
     <a href={CV} className={classList}>
       <span style={{ marginRight: "2px" }}>{buttonName}</span>
-      <span> {buttonImage}</span>
+      <span style={{ fontSize: "1.5rem" }}> {buttonImage}</span>
     </a>
   );
 }
